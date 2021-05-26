2e73b8fc26eef960f464a5702d1c1f6730bfe26d
blair488 <samantha99fan@gmail.com>
2021-05-26 15:27:30 +0800

    main.cpp

---
+++ demo/main.cpp
@@ -0,0 +1,65 @@
+#include "mbed.h"
+
+#define CENTER_BASE 1500
+
+BufferedSerial pc(USBTX, USBRX);
+DigitalIn encoder(D10);
+PwmOut servo(D11);
+
+Timer t;
+Ticker encoder_ticker;
+
+volatile int steps;
+volatile int last;
+
+void servo_control(int speed) {
+   if (speed > 200)       speed = 200;
+   else if (speed < -200) speed = -200;
+
+   servo=(CENTER_BASE + speed)/20000.0f;
+}
+
+void encoder_control() {
+   int value = encoder;
+   if(!last && value) steps++;
+   last = value;
+}
+
+
+int main() {
+
+   pc.set_baud(9600);
+
+   encoder_ticker.attach(&encoder_control, .01);
+
+   servo.period_ms(20);
+
+   while(1) {
+
+   //TODO: revise this value accordisudo mbed compile --source . --source ~/ee2405/mbed-os-build/ -m B_L4S5I_IOT01A -t GCC_ARM -fng to your result
+   servo_control(-22.208);
+
+   steps = 0;
+   t.reset();
+   t.start();
+
+   ThisThread::sleep_for(5000ms);
+
+   float time = t.read();
+
+   printf("clockwise:%1.3f\r\n", (float) steps * 6.5 * 3.14 / 32 / time);
+
+   servo_control(43.224);
+
+   steps = 0;
+   t.reset();
+   t.start();
+
+   ThisThread::sleep_for(5000ms);
+
+   time = t.read();
+
+   printf("counterclockwise:%1.3f\r\n", (float) steps * 6.5 * 3.14 / 32 / time);
+
+   }
+}
